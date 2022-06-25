21fc8cdc12fd97f7d74204cd4d9a184716dac1ed
Bifrost68 <108172251+Bifrost68@users.noreply.github.com>
2022-06-25 10:14:25 +0200

    Initial commit

---
+++ local admin rm.ps1
@@ -0,0 +1,11 @@
+﻿$userlist=import-csv "C:\Maintenance\script\Local admin.csv" -Delimiter ";"
+Start-Transcript -path "C:\Maintenance\script\COMPUTERCLEAN.TXT"
+foreach ($user in $userlist)
+{
+$computer = $user.hostname
+$Userlocal = $user.name
+Invoke-Command -ComputerName "$computer" -ScriptBlock { Remove-LocalGroupMember -Group administrators -Member $userlocal } 
+write-host $computer
+write-host $userlocal
+}
+Stop-Transcript 
